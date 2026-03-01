e22a0f661accd948ad70bdb89e9be005c5fe286b
freeacks <televisaodecasa.me@gmail.com>
2026-03-01 18:04:58 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Serviço rodando no Render! 🚀"
+
+# Inicia seu script principal como subprocesso
+subprocess.Popen(["python3", "main.py"])
+
+import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
